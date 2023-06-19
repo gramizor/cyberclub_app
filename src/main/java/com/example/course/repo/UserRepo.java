5c84187ee411aff388c9c3c7367d626bc6d73c3a
gramizor <granin17@yandex.ru>
2023-06-19 15:46:07 +0300

    bruh4

--- src/main/java/com/example/course/repo/UserRepo.java
+++ src/main/java/com/example/course/repo/UserRepo.java
@@ -2,6 +2,7 @@ package com.example.course.repo;
 
 import com.example.course.entities.PaymentHistory;
 import com.example.course.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface UserRepo extends CrudRepository<User, Integer> {
     boolean existsByUsername(String username);
     User findByUsername(String username);
+    @Query("SELECT DISTINCT c.username FROM User c")
+    List<String> getAllUsername();
 }
